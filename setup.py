05f7a05f6dfbe07b272e0e8c2edc880158728f81
Miical <879754743@qq.com>
2024-04-28 13:06:40 +0800

    Fixed the zero division error and updated setup

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ this_directory = path.abspath(path.dirname(__file__))
 with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     my_long_description = f.read()
 
-
 setup(
     # https://pypi.org/search/?q=&o=&c=Topic+%3A%3A+Software+Development+%3A%3A+Build+Tools
     classifiers=[
@@ -30,6 +29,11 @@ setup(
     url="https://github.com/XS-MLVP/mlvp",
 
     packages=find_packages(),
+
+      package_data={
+        'mlvp': ['templates/html/*',
+                 'templates/html/icons/*'],
+    },
 
     long_description_content_type="text/markdown",
 )
