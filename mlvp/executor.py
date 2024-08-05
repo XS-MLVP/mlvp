fdc9404e5bf05b160a6edbb18e3e77dfe65c12fe
Miical <879754743@qq.com>
2024-08-05 16:16:58 +0800

    Add wait_none, wait_all, wait_any

--- mlvp/executor.py
+++ mlvp/executor.py
@@ -1,4 +1,4 @@
-from .asynchronous import create_task, gather, add_callback
+from .asynchronous import create_task, gather, add_callback, Event
 from .base import MObject
 
 
@@ -14,6 +14,7 @@ def add_priority_task(coro, priority):
 async def __execute_priority_tasks():
     for coro, _ in sorted(__priority_tasks, key=lambda x: x[1]):
         await coro
+    __priority_tasks.clear()
 
 add_callback(__execute_priority_tasks)
 
@@ -23,26 +24,28 @@ Executor
 
 class Executor(MObject):
     def __init__(self, exit="all"):
-
-        import asyncio
         self.exit = exit
+        self.uncompleted = []
         self.results = {}
         self.coros = {}
 
+        self.__exit_any_event = Event()
 
     async def __aenter__(self):
         return self
 
     async def __aexit__(self, exc_type, exc_val, exc_tb):
-        await self._wait_for_all()
+        if self.exit == "all":
+            return await self.exit_all()
+        elif self.exit == "none":
+            return await self.exit_none()
+        elif self.exit == "any":
+            return await self.exit_any()
+        else:
+            raise ValueError(f"Invalid exit option: {self.exit}")
 
-    async def _wait_for_all(self):
-        sche_groups = []
-        for tasks in self.coros.items():
-            sche_groups.append(self.sequential_execution_all(*tasks[1]))
 
-        results = await gather(*sche_groups)
-
+    def set_results(self, results):
         result_dict = {}
         for i, tasks in enumerate(self.coros.items()):
             if len(tasks[1]) == 1:
@@ -52,6 +55,38 @@ class Executor(MObject):
 
         self.results = result_dict
 
+    def get_sche_group(self):
+        sche_groups = []
+        for tasks in self.coros.items():
+            sche_groups.append(self.sequential_execution_all(*tasks[1]))
+        return sche_groups
+
+    async def exit_all(self):
+        sche_groups = self.get_sche_group()
+        results = await gather(*sche_groups)
+        self.set_results(results)
+        return self.results
+
+    async def exit_none(self):
+        sche_groups = self.get_sche_group()
+        for coro in sche_groups:
+            self.uncompleted.append(create_task(coro))
+
+    async def exit_any(self):
+        self.__exit_any_event.clear()
+        for tasks in self.coros.items():
+            self.uncompleted.append(create_task(self.sequential_execution_all(\
+                *tasks[1], complete_event=self.__exit_any_event)))
+        await self.__exit_any_event.wait()
+
+    async def wait_all(self):
+        if len(self.uncompleted) == 0:
+            return
+
+        results = []
+        for task in self.uncompleted:
+            results.append(await task)
+        self.set_results(results)
 
     def get_results(self):
         return self.results
@@ -72,7 +107,7 @@ class Executor(MObject):
 
 
     @staticmethod
-    async def sequential_execution_all(*tasks):
+    async def sequential_execution_all(*tasks, complete_event=None):
         """Sequentially execute all tasks."""
 
         results = []
@@ -86,6 +121,9 @@ class Executor(MObject):
                 driver.priority = priority
 
             results.append(await coro)
+
+        if complete_event is not None and not complete_event.is_set():
+            complete_event.set()
 
         return results
 
