d22b7ebfb342803b616f52d9292eaf456544356b
Miical <879754743@qq.com>
2024-08-12 10:28:43 +0800

    Add reporter hook

--- mlvp/pytest_mlvp/plugin.py
+++ mlvp/pytest_mlvp/plugin.py
@@ -1,6 +1,13 @@
 import pytest
+from ..reporter import process_func_coverage, process_context
 
-@pytest.fixture
-def mlvp_test():
-    print("This is a test fixture")
-    return 1
+@pytest.hookimpl(trylast=True, optionalhook=True)
+def pytest_reporter_context(context, config):
+    process_context(context, config)
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    return process_func_coverage(item, call, report)
