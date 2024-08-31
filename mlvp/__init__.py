85c339b9d15cbc797ac58381130c3958e9ebbfde
Miical <879754743@qq.com>
2024-08-31 15:18:06 +0800

    Optimize directory structure

--- mlvp/__init__.py
+++ mlvp/__init__.py
@@ -1,23 +1,48 @@
+"""
+The mlvp package provides the core classes and functions for the mlvp framework.
+"""
+
 from .asynchronous import start_clock, create_task, run, gather
 from .asynchronous import Event, Queue, sleep
 from .asynchronous import Component
 
-from .triggers import *
-
 from .bundle import Bundle, WriteMode, Signal, Signals
 
-from .model import Model
-from .model import DriverPort, MonitorPort, AgentPort
-from .model import driver_hook, agent_hook
-
-from .agent import Agent, driver_method, monitor_method
-from .executor import Executor
-
-from .env import Env
-
-from . import logger
 from .logger import get_logger, setup_logging, summary
 from .logger import log, debug, info, warning, error, critical
 from .logger import INFO, DEBUG, WARNING, ERROR, CRITICAL
 
-from . import modules
+from .env import Env
+from .executor import Executor
+
+
+"""
+The triggers module privides trigger functions that are used to wait for a specific condition about clock event.
+
+content: ClockCycles, Value, AllValid, Condition, Change, RisingEdge, FallingEdge
+"""
+from . import triggers
+
+
+"""
+The model module provides the Model class, hook decorators, and port classes.
+
+content: Model, DriverPort, MonitorPort, AgentPort, driver_hook, and agent_hook
+"""
+from . import model
+
+
+"""
+The agent module provides the Agent class for operation on the DUT.
+
+content: Agent, driver_method, and monitor_method
+"""
+from . import agent
+
+
+"""
+utils provides some useful function for verification.
+
+content: lfsr_64, plru, two_bits_counter
+"""
+from . import utils
